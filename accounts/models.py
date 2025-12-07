605d068ae4421f3cefe98583bf8d8f2542a16de6
prateek <mani@bhagyamanii.localdomain>
2025-12-07 16:47:02 +0000

    add user_id - userModel

--- accounts/models.py
+++ accounts/models.py
@@ -6,6 +6,7 @@ import uuid
 
 class User(SoftDeleteModel, AbstractUser):
    
+    user_id = models.CharField(max_length=30, unique=True, editable=False, null=True)
     email = models.EmailField(unique=True)
     full_name = models.CharField(max_length=255, blank=True)
     session_token = models.UUIDField(default=uuid.uuid4, editable=False)
@@ -16,6 +17,13 @@ class User(SoftDeleteModel, AbstractUser):
     objects = UserManager()
     all_objects = models.Manager()
 
+    def save(self, *args, **kwargs):
+        if not self.user_id:
+            last = User.all_objects.order_by("id").last()
+            next_id = 1 if not last else last.id + 1
+            self.user_id = f"USR{next_id}"
+        super().save(*args, **kwargs)
+        
     def __str__(self):
         return self.email
 
